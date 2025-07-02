4a5d831c61444898a1db39704cca2980e0b03be3
USER <USER@DESKTOP-JG0GFCN>
2025-07-02 17:44:49 +0900

    Promise.anyの記述例

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,21 +1,22 @@
-// resolve()を100ミリ秒後に実行するPromiseインスタンス
+// resolve()を200ミリ秒後に実行するPromiseインスタンス
 const myResolve = new Promise(resolve => {
     setTimeout(() => {
         resolve("resolveが呼ばれました。");
         console.log("myResolveの実行が終了しました。");
-    }, 300); // rejectと時間を逆にすると順番が変わる
+    }, 200);
 });
 
-// reject()を200ミリ秒後に実行するPromiseインスタンス
+// reject()を100ミリ秒後に実行するPromiseインスタンス
+// rejectがmyResolveのresolveより前に呼び出される
 const myReject = new Promise((_, reject) => {
     // resolveを使わない為 _ としておく
     setTimeout(() => {
         reject("rejectが呼ばれました。");
         console.log("myRejectの実行が終了しました。");
-    }, 200);
+    }, 100);
 });
 
-Promise.race([myReject, myResolve])
+Promise.any([myReject, myResolve])
     .then(value => {
         console.log(value);
     }).catch(value => {
