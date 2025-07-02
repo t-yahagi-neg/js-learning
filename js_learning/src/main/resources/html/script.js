8e0d3005f0b4ce6e7fcac53a10fe7cfc10480752
USER <USER@DESKTOP-JG0GFCN>
2025-07-02 16:34:29 +0900

    １ずつインクリメントした値を返すジェネレータ

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,18 +1,15 @@
-function* gen1to3() {
-    let index = 1;
-    yield index;    // 1回目のnext()によってこの地点まで実行が完了
-    index++;
-    yield index;    // 2回目のnext()によってこの地点まで実行が完了
-    index++;
-    return index;   // 3回目のnext()によってこの地点まで実行が加療
+function* genIterator(max) {
+    let value = 0;
+    while(value < max) {
+        yield value++;
+    }
 }
 
-// ジェネレータ関数の実行はGeneratorオブジェクトの生成を意味する
-const generator = gen1to3();
+const iterator = genIterator(5);
 
-// next()の実行によってジェネレータ関数内のコードが実行される
-console.log(generator.next());
-
-console.log(generator.next());
-
-console.log(generator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
