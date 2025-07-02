bb27b22ee565bccd2abf94097df0373c97447456
USER <USER@DESKTOP-JG0GFCN>
2025-07-02 16:55:17 +0900

    JavaScriptのコードがシングルスレッドで実行されることを証明

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,13 +1,17 @@
-function sum(...vals) {
-    let returnValue = 0;
+function sleep(ms) {
+    const startTime = new Date();
+    // whileループでmsミリ秒分ループを継続
+    while(new Date() - startTime < ms); // msミリ秒経つまでは条件がtrueにならない
+    // whileのブロック{ }は省略
 
-    for(const val of vals) {
-        returnValue += val;
-    }
-
-    return returnValue;
+    alert("sleep関数が完了しました。"); // whileループが完了した直後にダイアログを表示
 }
 
-console.log(sum(1,2));
-console.log(sum(1,2,3));
-console.log(sum(1,2,3,4));
+sleep(3000);    // sleep関数を実行
+
+// ボタンにクリック時の処理を実装
+const btn = document.querySelector("button");
+function clickHandler() {
+    alert("ボタンがクリックされました。");
+}
+btn.addEventListener("click", clickHandler);    // 「ボタン」にアクションを登録
