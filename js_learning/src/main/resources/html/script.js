0ce27c0e06a446636c68d4733985aaf962e07662
USER <USER@DESKTOP-JG0GFCN>
2025-07-02 17:31:01 +0900

    Promise.allの記述例

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,29 +1,18 @@
-function promiseFactory(count) {
+function wait(ms) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            // 渡されてきたcountの値をインクリメント
-            count++;
-
-            console.log(`${count}回目のコールです。時刻：[${new Date().toTimeString()}]`);
-
-            // ３回目のコールでエラー
-            if(count === 3) {
-                reject(count);
-            } else {
-                // 次のthenのコールバック関数の引数にcountが渡る
-                resolve (count);
-            }
-        }, 1000);
+            console.log(`${ms}msの処理が完了しました。`);
+            resolve(ms);
+        }, ms); // 引数のms分だけ待機
     });
 }
 
-promiseFactory(0)
-.then(count => {return promiseFactory(count);})
-.then(count => {return promiseFactory(count);})
-.then(count => {return promiseFactory(count);})
-.then(count => {return promiseFactory(count);})
-.catch(errorCount => {
-    console.log(`エラーに飛びました。現在のカウントは${errorCount}です。`);
-}).finally(() => {
-    console.log("処理を終了します。");
-});
+const wait400 = wait(400);
+const wait500 = wait(500);
+const wait600 = wait(600);
+
+Promise.all([wait500, wait600, wait400])
+    .then(([resolved500, resolved600, resolved400]) => {
+        console.log("すべてのPromiseが完了しました。");
+        console.log(resolved500, resolved600, resolved400)
+    });
