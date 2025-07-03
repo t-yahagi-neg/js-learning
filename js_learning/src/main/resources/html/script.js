9adcb7989f0a792785cb60dfbaea8701aaac0c87
USER <USER@DESKTOP-JG0GFCN>
2025-07-03 10:31:46 +0900

    特定のElementを取得するメソッド

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,44 +1,4 @@
-function promiseFactory(count) { // この関数はawait / asyncで書き換えることはできない
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            count++;
-
-            console.log(`${count}回目のコールです。時刻：[${new Date().toTimeString()}]`);
-
-            if(count === 3) {
-                reject(count);
-            } else {
-                resolve(count);
-            }
-        }, 1000);
-    });
-}
-
-/* このコードをexecute()で書き換える
-promiseFactori(0)
-.then(count=> {return promiseFactory(count);})
-.then(count=> {return promiseFactory(count);})
-.then(count=> {return promiseFactory(count);})
-.catch(errorCount => {
-    console.error(`エラーに飛びました。現在のカウントは${errorCount}です。);
-}).finally(() => {
-    console.log("処理を終了します。");
-});
-*/
-
-// await / asyncを使った書き換え
-async function execute() {  // awaitを内部で使っているためasyncを付ける
-    try {
-        //promiseFactory内のresolveが呼び出されるまで次の処理を実行しない
-        let count = await promiseFactory(0);    // awaitによってresolveの引数の値がcountに代入される
-        count = await promiseFactory(count);
-        count = await promiseFactory(count);
-        count = await promiseFactory(count);
-    } catch (errorCount) {
-        console.log(`エラーに飛びました。現在のカウントは${errorCount}です。`); // Promiseがrejectedのステータスになった場合はcatchブロックに遷移する
-    } finally {
-        console.log("処理を終了します。");
-    }
-}
-
-execute();  // execute()の実行
+const elById = document.getElementById("container");
+const elsByCls = document.getElementsByClassName("target-cls");
+const elsByName = document.getElementsByName("child1");
+const elsByTag = document.getElementsByTagName("p");
