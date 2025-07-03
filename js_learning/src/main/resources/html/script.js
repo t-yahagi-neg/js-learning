c01d4a11f8208324301c751e7ba649c9c005a483
USER <USER@DESKTOP-JG0GFCN>
2025-07-03 15:36:01 +0900

    createElementを使った要素の作成

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,6 +1,6 @@
-const testEl = document.querySelector("#test");
-console.log(`innerHTML: ${testEl.textContent}`);
-
-setTimeout(() => {
-    testEl.textContent = "<h1>Good World</h1>";
-}, 2000);
+// 新しい <div> 要素を作成
+const newDiv = document.createElement("div");
+// 作成した <div> 要素に文字列を追加
+newDiv.textContent = "Hello World";
+// <body> に挿入
+document.body.prepend(newDiv);
