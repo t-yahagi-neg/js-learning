09195783b40fca8dce8ed5659b4519ea0c3593c1
USER <USER@DESKTOP-JG0GFCN>
2025-07-03 10:54:03 +0900

    DOMツリー構築後にコードが実行される

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,5 +1,5 @@
-const elById = document.querySelector("container");
-const elsByCls = document.querySelectorAll("target-cls");
-const elByCls = document.querySelector("target-cls");
-const elsByName = document.querySelectorAll("child1");
-const elsByTag = document.querySelector("p");
+document.addEventListener("DOMContentLoaded", () => {
+    /* この関数内のコードはDOMツリー全体の構築が完了してから実行されます。 */
+    const afterEl = document.querySelector("#after");
+    console.log(afterEl.textContent);
+})
