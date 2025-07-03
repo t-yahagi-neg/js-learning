acdc49efc1d7db2a77ee72082033499b523e1e63
USER <USER@DESKTOP-JG0GFCN>
2025-07-03 15:46:00 +0900

    複雑なHTML構造のElementを作成する場合

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,6 +1,28 @@
-// 新しい <div> 要素を作成
-const newDiv = document.createElement("div");
-// 作成した <div> 要素に文字列を追加
-newDiv.textContent = "Hello World";
-// <body> に挿入
-document.body.prepend(newDiv);
+// <body> に挿入したいHTMLの構造を文字列で定義
+const htmlStr = `
+    <article id="article">
+        <h1 id="article-title">記事のタイトル</h1>
+            <div class="tag-area">
+                <span>タグ：</span><span>スポーツ</span><span>バスケ</span>
+            </div>
+            <div class="article-body">記事の本文</div>
+            <div id="recommend">
+                <h2>おすすめの記事</h2>
+                <a href="#">他の記事</a>
+            </div>
+        </article>
+    `;
+
+// HTML文字列をElementオブジェクトに変換する関数
+function htmlStrToElement(htmlStr) {
+    // ダミーの<div>要素を作成
+    const dummyDiv = document.createElement("div");
+    // HTML文字列がElementに変換される
+    dummyDiv.innerHTML = htmlStr;
+    // ダミーの<div>要素の子要素を返す
+    return dummyDiv.firstElementChild;
+}
+
+// HTML文字列からElementオブジェクトを取得
+const targetNewElement = htmlStrToElement(htmlStr);
+document.body.prepend(targetNewElement);
