85746e63eb00575d982e6bc3c25c1b580bceb54f
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-07 23:26:19 +0900

    アンカータグのリンク先を Google に変更

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,6 +1,2 @@
-const ul = document.querySelector("ul");
-const li = ul.querySelector("li");
-for (let i = 0; i < 3; i++) {
-    const clone = li.cloneNode(true);   // 要素を複製
-    ul.append(clone);
-}
+const link = document.querySelector("a");
+link.setAttribute("href", "https://google.com");
