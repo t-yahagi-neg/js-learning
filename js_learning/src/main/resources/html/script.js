4048ccbdaf254a02485bc5b554c3e837421de07b
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-07 23:33:59 +0900

    データ属性の取得・変更

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,2 +1,6 @@
-const link = document.querySelector("a");
-link.setAttribute("href", "https://google.com");
+const sum = document.querySelector("#sum");
+const val1 = parseInt(sum.dataset.baseVal1);    // データ属性値を取得
+const val2 = parseInt(sum.dataset.baseVal2);    // "-"はキャメルケースとして表現する
+
+sum.dataset.sumVal = val1 + val2;   // データ属性の追加も可能
+sum.textContent = val1 + val2;  // 結果をテキストとして追加
