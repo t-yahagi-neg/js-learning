63fcb9a7b632619465a1a1d352d1877a88e662ae
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-08 11:27:33 +0900

    アクションの解除がうまくいかない例

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,28 +1,11 @@
 const targetEl = document.querySelector("div");
 const colorBtn = document.querySelector("#color");
-const bgColorBtn = document.querySelector("#bg-color");
-const allColorBtn = document.querySelector("#all-color");
-const resetBtn = document.querySelector("#reset-color");
-// 文字色を変更するアクション
-function colorChange(event) {
-    // 文字色を赤色に変更
-    targetEl.style.color = "#ff0000";
-}
-// 背景色を変更するアクション
-function bgColorChange(event) {
-    // 背景色を青色に変更
-    targetEl.style.backgroundColor = "blue";
-}
-// スタイルをリセットするアクション
-function reset(event) {
-    targetEl.style.backgroundColor = "";
-    targetEl.style.color = "";
-}
 
-// イベントリスナにアクションを登録
-colorBtn.addEventListener("click", colorChange);
-bgColorBtn.addEventListener("click", bgColorChange);
-allColorBtn.addEventListener("click", colorChange);     // #all-color要素には関数を2つ登録
-allColorBtn.addEventListener("click", bgColorChange);   //
-// スタイルのリセット
-resetBtn.addEventListener("click", reset);
+// アクションの登録：無名関数A
+colorBtn.addEventListener("click", function(even) {
+    targetEl.style.color = "#ff0000";
+});
+// アクションの解除（解除できていない）：無名関数B
+colorBtn.removeEventListener("click", function(event){
+    targetEl.style.color = "#ff0000";
+});
