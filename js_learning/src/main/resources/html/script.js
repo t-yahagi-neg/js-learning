9efc1a96de473a05b3457b3277abda572494694d
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-08 10:25:33 +0900

    class 属性をセレクタで指定してスタイルを適用

--- js_learning/src/main/resources/html/script.js
+++
@@ -1,5 +0,0 @@
-setTimeout(() => {
-    const div = document.querySelector("div");
-    div.style.color = "red";    /* 文字色を赤色に変更 */
-    div.style.backgroundColor = "blue"; /* 背景色を青色に変更 */
-}, 2000);   /* ２秒後にコールバック関数を実行 */
