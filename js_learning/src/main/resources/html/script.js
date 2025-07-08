84c60f35f793182309332e65958b60224621317a
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-08 10:00:48 +0900

    レイアウト幅とレンダリング幅の違い

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,6 +1,4 @@
-const sum = document.querySelector("#sum");
-const val1 = parseInt(sum.dataset.baseVal1);    // データ属性値を取得
-const val2 = parseInt(sum.dataset.baseVal2);    // "-"はキャメルケースとして表現する
+const div = document.querySelector("div");
 
-sum.dataset.sumVal = val1 + val2;   // データ属性の追加も可能
-sum.textContent = val1 + val2;  // 結果をテキストとして追加
+console.log(div.offsetHeight);  // offset プロパティはレイアウト幅を取得する
+console.log(div.getBoundingClientRect().height);    // getBoundingClientRect はレンダリング幅を取得する
