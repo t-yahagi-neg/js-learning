6aaf2d8cdac4ec11fe4865102a8dc57a0a9af2f7
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-08 10:21:09 +0900

    style プロパティから文字色と背景色を変更

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,4 +1,5 @@
-const div = document.querySelector("div");
-
-console.log(div.offsetHeight);  // offset プロパティはレイアウト幅を取得する
-console.log(div.getBoundingClientRect().height);    // getBoundingClientRect はレンダリング幅を取得する
+setTimeout(() => {
+    const div = document.querySelector("div");
+    div.style.color = "red";    /* 文字色を赤色に変更 */
+    div.style.backgroundColor = "blue"; /* 背景色を青色に変更 */
+}, 2000);   /* ２秒後にコールバック関数を実行 */
