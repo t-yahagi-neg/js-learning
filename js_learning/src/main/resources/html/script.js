ebaee51a4157adc75755776d6d309a416edab447
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-08 11:29:27 +0900

    アクションの解除がうまくいく例

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,11 +1,11 @@
 const targetEl = document.querySelector("div");
 const colorBtn = document.querySelector("#color");
 
-// アクションの登録：無名関数A
-colorBtn.addEventListener("click", function(even) {
+function colorChange(event) {
     targetEl.style.color = "#ff0000";
-});
-// アクションの解除（解除できていない）：無名関数B
-colorBtn.removeEventListener("click", function(event){
-    targetEl.style.color = "#ff0000";
-});
+}
+
+// アクションの登録
+colorBtn.addEventListener("click", colorChange);
+// アクションの解除（うまくいく）
+colorBtn.removeEventListener("click", colorChange);
