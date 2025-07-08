3ca9f907cd227ab55e755a267b1c6aa18e1e0d7d
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-08 10:59:12 +0900

    ボタンに登録したアクションを解除

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,17 +1,5 @@
-// 数値の初期化
-let count = 0;
-
-// Elementの取得
-const number = document.querySelector("#number");
-const plusBtn = document.querySelector("#plus");
-const minusBtn = document.querySelector("#minus");
-
-plusBtn.onclick = function(event) { // #plus ボタンのクリック時のアクションを定義
-    count++; // count に 1 を加算
-    number.textContent = count; // #number のテキストを更新
-};
-
-minusBtn.onclick = function(event) { // #minus ボタンのクリック時のアクションを定義
-    count--;    // count から 1 を減産
-    number.textContent = count; // #number のテキストを更新
-};
+const btn = document.querySelector("button");
+btn.onclick = function() {  // アクションの追加
+    alert("アラート！");
+}
+btn.onclick = null; // アクションの解除
