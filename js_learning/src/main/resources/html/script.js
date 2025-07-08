927637b7ef6dc710d4732ea6e945a64a78a83e4e
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-08 11:14:13 +0900

    イベントリスナにアクションを登録

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,5 +1,17 @@
-const btn = document.querySelector("button");
-btn.onclick = function() {  // アクションの追加
-    alert("アラート！");
-}
-btn.onclick = null; // アクションの解除
+// 数値の初期化
+let count = 0;
+
+// Elementの取得
+const number = document.querySelector("#number");
+const plusBtn = document.querySelector("#plus");
+const minusBtn = document.querySelector("#minus");
+
+plusBtn.addEventListener("click", function(event) { // クリックイベントにアクションを登録
+    count++; // 数値を 1 加算する
+    number.textContent = count; // #number のテキストを更新
+});
+
+minusBtn.addEventListener("click", function(event) { // クリックイベントにアクションを登録
+    count--;    // 数値を 1 減算する
+    number.textContent = count; // #number のテキストを更新
+});
