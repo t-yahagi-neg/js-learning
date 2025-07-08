b9b6d11ec472022dfd9872f69e997c31b6938c5b
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-08 12:06:10 +0900

    Event.stopImmediatePropagationの使用例

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,19 +1,18 @@
-const button = document.querySelector("button");
-const div = document.querySelector("div");
-const section = document.querySelector("section");
+const targetEl = document.querySelector("div");
+const allColorBtn = document.querySelector("#all-color");
 
-// button に対するアクションを登録
-button.addEventListener("click", (event) => {
-    console.log("button の click イベントが実行されました。");
-});
+// 文字色を変更するアクション
+function colorChange(event) {
+    event.stopImmediatePropagation();   // 皇族のアクションへのイベント伝播を停止
+    targetEl.style.color = "#ff0000";   // 文字色を赤色に変更
+}
 
-// div に対するアクションを登録
-div.addEventListener("click", (event) => {
-    console.log("div の click イベントが実行されました。");
-});
+// 背景色を変更するアクション
+function bgColorChange(event) {
+    targetEl.style.backgroundColor = "blue";    // 背景色を青色に変更
+}
 
-// section に対するアクションを登録
-section.addEventListener("click", (event) => {
-    event.stopPropagation();    // イベント伝播を停止
-    console.log("section の click イベントが実行されました。");
-}, {capture: true});    // キャプチャリングフェーズアクションで実行
+// 関数を2つ登録可能
+// アクションは登録された順番 (colorChange → bgColorChange) で実行される
+allColorBtn.addEventListener("click", colorChange);
+allColorBtn.addEventListener("click", bgColorChange)
