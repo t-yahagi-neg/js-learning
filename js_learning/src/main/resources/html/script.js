3e4d638a03754b0785d57e4e4c2c05d40337c318
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-08 13:42:48 +0900

    <div>要素、<button>要素に対してアクションを登録

--- js_learning/src/main/resources/html/script.js
+++ js_learning/src/main/resources/html/script.js
@@ -1,17 +1,4 @@
-const button = document.querySelector("button");
-const span = document.querySelector("span");
-
-let count = 0;
-button.addEventListener("click", function(event) {
-    count++;
-    span.textContent = count;
-});
-
-// Event オブジェクトをインスタンス化
-const myEvent = new Event("click");
-
-// 1 秒間隔で click イベントを発火
-setInterval(() => {
-    // イベントを発火
-    button.dispatchEvent(myEvent);
-}, 1000);
+const btn = document.querySelector("button");
+const div = document.querySelector("div");
+btn.addEventListener("click", () => { console.log("button") });
+div.addEventListener("click", () => { console.log("div") });
