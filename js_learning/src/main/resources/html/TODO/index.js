8e49d7c71ff9beb33ca50222188b32b7b3b872f6
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 23:16:28 +0900

    ローカルストレージからデータを取得する

--- js_learning/src/main/resources/html/TODO/index.js
+++ js_learning/src/main/resources/html/TODO/index.js
@@ -1,16 +1,28 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
+// ローカルストレージから値を取得、JSONから配列に
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+// todosが空でなければ
+if (todos) {
+    todos.forEach(todo => {
+        add(todo);
+    })
+}
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     add();
 });
 
-function add() {
+function add(todo) {
     let todoText = input.value;
-    // フォームが空欄の時は追加しないようにする
-    // 暗黙的型変換で未入力なら空文字になるためfalseになる
+    
+    if (todo) {
+        todoText = todo;
+    }
+
     if (todoText) {
         const li = document.createElement("li");
         li.innerText = todoText;
@@ -27,6 +39,6 @@ function saveData() {
     lists.forEach(list => {
         todos.push(list.innerText);
     });
-    // ローカルストレージに保管、JSON型にする
+    // ローカルストレージに保存する、JSON型にする
     localStorage.setItem("todos", JSON.stringify(todos));
 }
