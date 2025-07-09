958f5e930b255783204ec483021fc0285404a5d5
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 22:37:03 +0900

    idタグの要素を取得し、コンソールに値を出力する

--- js_learning/src/main/resources/html/TODO/index.js
+++ js_learning/src/main/resources/html/TODO/index.js
@@ -1,2 +1,11 @@
-// コメント
-console.log('Hello')
+// id="form" を指定
+const form = document.getElementById("form");
+// id="input" を指定
+const input = document.getElementById("input");
+
+// formでEnter(submit)した時のイベント
+form.addEventListener("submit", function() {
+    // formのinputの値を取得する
+    // inputの値が正しく取得できているかをコンソールに出力して確認する
+    console.log(input.value);
+});
